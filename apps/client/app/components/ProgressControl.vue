69701b91a8673d3ffec1df6a991a74c84129f7b6
khiem <khiemnguyen.hye@gmail.com>
2026-01-16 23:38:34 +0700

    Scaffold progress bar and progress control

--- apps/client/app/components/ProgressControl.vue
+++ apps/client/app/components/ProgressControl.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="fixed bottom-0 h-15 w-screen border-t">
-    <div class="flex max-w-7xl">
-      <div class="flex flex-1">
-        <Button>Previous</Button>
+  <div class="flex fixed bottom-0 h-15 w-screen border-t">
+    <div class="flex justify-between items-center w-2xl mx-auto">
+      <div class="flex flex-1 justify-start">
+        <Button variant="ghost">
+          <Icon name="material-symbols:arrow-back-ios-rounded" /> Previous
+        </Button>
+      </div>
+      <div class="flex flex-1 justify-center">
+        Someshit
+      </div>
+      <div  class="flex flex-1 justify-end">
+        <Button variant="ghost">
+          Next <Icon name="material-symbols:arrow-forward-ios-rounded" />
+        </Button>
       </div>
     </div>
   </div>

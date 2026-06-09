f05f1d6ac4836d1dbf79cce84b6deddce7853226
khiem <khiemnguyen.hye@gmail.com>
2026-06-09 18:12:22 +0800

    Add stuff and enable devtool

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
     },
   },
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: false },
+  devtools: { enabled: true },
   modules: ["shadcn-nuxt", "@nuxt/icon"],
   runtimeConfig: {
     public: {

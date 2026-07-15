b1b798c1992cec3df0d81e9db6c2be3a2aa58f54
khiem <khiemnguyen.hye@gmail.com>
2026-07-15 16:32:02 +0800

    TEMP: fit image into browse page

--- apps/client/app/pages/browse.vue
+++ apps/client/app/pages/browse.vue
@@ -7,19 +7,28 @@ definePageMeta({ layout: "internal", middleware: "auth" });
       class="p-5 pb-4 pl-6 border-b border-primary/5 flex items-center justify-between"
     >
       <h1 class="text-xl font-black text-primary flex items-center gap-2">
-        Matches
-        <span
-          class="text-xs font-bold px-2 py-0.5 rounded-full bg-secondary text-muted-foreground"
-        >
-          Some number
-        </span>
+        Browse
       </h1>
+      <!-- TODOO: Implement filter here -->
       <Button
         variant="outline"
         class="rounded-xl h-9 px-4 font-bold border-primary/10 hover:bg-primary hover:text-primary-foreground transition-all"
       >
-        New
+        Filter
       </Button>
     </div>
+    <ScrollArea class="flex-1 min-h-0">
+      <div class="p-5">
+        <div
+          class="flex items-center gap-4 p-2 rounded-md bg-secondary/30 hover:bg-secondary/60 transition-all cursor-pointer border border-transparent hover:border-primary/5"
+        >
+          <img
+            src="https://i.pinimg.com/736x/67/0d/a7/670da7c3f9cc857613346cecd5ecba26.jpg"
+            alt="Profile"
+            class="rounded-sm"
+          >
+        </div>
+      </div>
+    </ScrollArea>
   </div>
 </template>

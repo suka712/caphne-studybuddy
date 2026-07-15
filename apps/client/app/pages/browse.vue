087b3dd080e04d491ef8c9ff9b3397f025914de2
khiem <khiemnguyen.hye@gmail.com>
2026-07-15 19:51:51 +0800

    TEMP Fetch and display with typesafety on /browse

--- apps/client/app/pages/browse.vue
+++ apps/client/app/pages/browse.vue
@@ -1,8 +1,50 @@
 <script setup lang="ts">
 definePageMeta({ layout: "internal", middleware: "auth" });
+
+type NextCursor = {
+  updatedAt: Date;
+  id: string;
+} | null;
+
+type BrowseProfile = {
+  displayName: string;
+  id: string;
+  major: string;
+  photoUrl: string;
+}
+
+type BrowseProfilesResponse = {
+  profiles: BrowseProfile[];
+  cursor: NextCursor;
+}
+
+const isLoading = ref(true);
+const browseProfiles = ref<BrowseProfile[]>([]);
+
+const {
+  public: { apiBase },
+} = useRuntimeConfig();
+
+const fetchBrowseProfiles = async () => {
+  const data = await $fetch<BrowseProfilesResponse>(`${apiBase}/browse`, {
+    credentials: "include",
+  });
+
+  browseProfiles.value = data.profiles;
+  isLoading.value = false;
+};
+
+onMounted(async () => {
+  await fetchBrowseProfiles();
+
+  console.log(browseProfiles.value);
+});
 </script>
 <template>
-  <div>
+  <div v-if="isLoading">
+    <div>Loading</div>
+  </div>
+  <div v-else>
     <div
       class="p-5 pb-4 pl-6 border-b border-primary/5 flex items-center justify-between"
     >
@@ -17,17 +59,34 @@ definePageMeta({ layout: "internal", middleware: "auth" });
         Filter
       </Button>
     </div>
-    <ScrollArea class="flex-1 min-h-0">
-      <div class="p-5">
-        <div
-          class="flex items-center gap-4 p-2 rounded-md bg-secondary/30 hover:bg-secondary/60 transition-all cursor-pointer border border-transparent hover:border-primary/5"
+    <ScrollArea class="flex min-h-0">
+      <!-- TODOO: Implement /browse/id -->
+      <div class="flex p-5 gap-3">
+        <NuxtLink
+          v-for="browseProfile in browseProfiles"
+          :key="browseProfile.id"
+          :to="`/browse/${browseProfile.id}`"
+          class="flex gap-3"
         >
-          <img
-            src="https://i.pinimg.com/736x/67/0d/a7/670da7c3f9cc857613346cecd5ecba26.jpg"
-            alt="Profile"
-            class="rounded-sm"
+          <div
+            class="items-center p-2 rounded-md bg-secondary/30 hover:bg-secondary/60 transition-all cursor-pointer border border-transparent hover:border-primary/5"
           >
-        </div>
+            <img
+              v-if="browseProfile.photoUrl"
+              :src="browseProfile.photoUrl"
+              alt="Profile"
+              class="rounded-sm shadow-md"
+            />
+            <Icon
+              v-else
+              name="mdi:account"
+              size="24"
+              class="text-muted-foreground"
+            />
+            <div class="font-bold pt-1">{{ browseProfile.displayName }}</div>
+            <div class="text-sm">{{ browseProfile.major }}</div>
+          </div>
+        </NuxtLink>
       </div>
     </ScrollArea>
   </div>

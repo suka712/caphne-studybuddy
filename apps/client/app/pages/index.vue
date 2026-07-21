f68d6bc10a9a29845a74672869b1125367cc4042
khiem <khiemnguyen.hye@gmail.com>
2026-07-22 00:02:08 +0800

    Remove SSR for real

--- apps/client/app/pages/index.vue
+++ apps/client/app/pages/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 definePageMeta({
   layout: "default",
-  ssr: false
 });
 
 const {
--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   routeRules: {
-    "/": { ssr: true },
+    "/": { ssr: false },
     "/chat/**": { ssr: false },
     "/discover": { ssr: false },
     "/matches": { ssr: false },
